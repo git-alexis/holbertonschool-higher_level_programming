e010ad7a8d528a7a5d88252faa074414ea675db4
git-alexis <alex59.billemont@gmail.com>
2024-01-23 10:39:53 +0100

    Add function to convert string to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
+def uppercase(str):
+    for letter in str:
+        if islower(letter):
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end="")
+    print("")
