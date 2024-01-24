d8e944cf879f857dbb585d24cbe3d5ae419ef728
git-alexis <alex59.billemont@gmail.com>
2024-01-24 10:58:57 +0100

    Add handling for single argument in command line

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
 
     if len(argv) == 1:
         print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for index in range(1, len(argv)):
