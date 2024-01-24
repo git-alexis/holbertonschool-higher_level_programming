38d842cd81eed808f76211e415f956efcb635f05
git-alexis <alex59.billemont@gmail.com>
2024-01-24 11:23:23 +0100

    Refactor print_names_from_pyc function to store names in a list before printing

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-import marshal, dis
+import marshal
 
 def print_names_from_pyc(file_path):
+    names = []
     with open(file_path, 'rb') as file:
         magic = file.read(4)
         timestamp = file.read(4)
@@ -12,7 +13,10 @@ def print_names_from_pyc(file_path):
             if isinstance(instruction, type(code)):
                 for name in instruction.co_names:
                     if not name.startswith('__'):
-                        print(name)
+                        names.append(name)
+
+    for name in sorted(names):
+        print(name)
 
 if __name__ == "__main__":
     print_names_from_pyc("hidden_4.pyc")
