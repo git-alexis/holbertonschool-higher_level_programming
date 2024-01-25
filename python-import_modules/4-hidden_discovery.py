17b345084848d7c40c695192078a54fced2621b1
git-alexis <alex59.billemont@gmail.com>
2024-01-25 09:53:36 +0100

    Refactor hidden module import and print names from module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-
-import marshal
-
-def print_names_from_pyc(file_path):
-    names = []
-    with open(file_path, 'rb') as file:
-        magic = file.read(4)
-        timestamp = file.read(4)
-        code = marshal.load(file)
-
-        for instruction in code.co_consts:
-            if isinstance(instruction, type(code)):
-                for name in instruction.co_names:
-                    if not name.startswith('__'):
-                        names.append(name)
-
-    for name in sorted(names):
-        print(name)
-
 if __name__ == "__main__":
-    print_names_from_pyc("hidden_4.pyc")
+
+    import hidden_4
+
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
