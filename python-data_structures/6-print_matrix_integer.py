79944f09e652897c28e4c83d1866872975f81938
git-alexis <alex59.billemont@gmail.com>
2024-01-30 11:13:11 +0100

    Fix print_matrix_integer function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,8 +5,9 @@ def print_matrix_integer(matrix=[[]]):
         for line in matrix:
             for index in range(len(line)):
                 if index == len(line) - 1:
-                    print("{:d}".format(line[index]))
+                    print("{:d}".format(line[index]), end="")
                 else:
                     print("{:d}".format(line[index]), end=" ")
+            print()
     else:
         print()
