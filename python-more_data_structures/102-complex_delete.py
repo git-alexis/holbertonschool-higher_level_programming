fa1c8e90e809c7515b3ceda07044cd5c659b0aa8
git-alexis <alex59.billemont@gmail.com>
2024-02-01 15:51:08 +0100

    Fix complex_delete function to use dot notation for dictionary deletion

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,5 +4,5 @@ def complex_delete(a_dictionary, value):
     if a_dictionary:
         for key, val in list(a_dictionary.items()):
             if val == value:
-                del(a_dictionary[key])
+                del a_dictionary[key]
     return a_dictionary
