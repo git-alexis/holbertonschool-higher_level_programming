ce7a5ee29fb99b6ed835180fae4288297e8b5886
git-alexis <alex59.billemont@gmail.com>
2024-02-06 11:26:31 +0100

    Add Square class with size attribute and exception of type and value

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""class that defines a Square"""
+
+
+class Square:
+
+    """Initialise the class with a private instance attribute size"""
+
+    def __init__(self, size=0):
+        """Check execption tupe and value for size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
