ea5653b960cb80c0106bc61f27f9af15887450c7
git-alexis <alex59.billemont@gmail.com>
2024-02-08 10:12:08 +0100

    Fix size and position validation in Square class

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -23,7 +23,8 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -35,9 +36,12 @@ class Square:
         """Set the position of the square"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(num, int) and num >= 0 for num in value):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        if value[0] < 0 or value[1] < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
         """Return the area of the square"""
@@ -48,7 +52,8 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
