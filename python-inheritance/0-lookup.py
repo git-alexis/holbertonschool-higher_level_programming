4e5cbfc77f8b2d270811bdf1b543b411777c7958
git-alexis <alex59.billemont@gmail.com>
2024-02-14 17:29:18 +0100

    fix doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+""" lookup function """
+
+
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """ Returns list of available attributes and methods of an object
+    Args:
+        obj: object to look up
+    Returns:
+        list of attributes and methods
+     """
+
     return dir(obj)
