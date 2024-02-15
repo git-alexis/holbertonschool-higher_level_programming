c0f9b1a87a5882013b0db0f8ccf169756bf8c3e4
git-alexis <alex59.billemont@gmail.com>
2024-02-15 14:37:15 +0100

    Add MyList class with print_sorted method

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" MyList class """
+
+
+class MyList(list):
+
+    """ MyList class """
+
+    def print_sorted(self):
+        """ prints the list, but sorted """
+        print(sorted(self))
