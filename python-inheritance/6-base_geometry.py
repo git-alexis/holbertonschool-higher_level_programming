37a59e899dfe46822c64f7e6f615436d9e0da2a9
git-alexis <alex59.billemont@gmail.com>
2024-02-15 16:43:18 +0100

    Add BaseGeometry class with area() method that raises an Exception with the message area() is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" BaseGeometry class """
+
+
+class BaseGeometry:
+
+    """ Empty __init__ method class """
+
+    def area(self):
+        """ Raises an Exception with the message: area() is not implemented """
+        raise Exception("area() is not implemented")
