17a42592e0f1f2aff2f0f4825d9203d6d0527b23
git-alexis <alex59.billemont@gmail.com>
2024-02-16 10:39:02 +0100

    Add integer_validator() methods that checks if a value is type int and greater than 0

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Validates value if it's an integer > 0 """
+        """ Validates value if it's an integer greater than 0 """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
