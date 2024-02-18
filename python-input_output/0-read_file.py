9aada307692ad6c55ac0a3c5f81bc6848aa2e562
git-alexis <alex59.billemont@gmail.com>
2024-02-18 23:31:32 +0100

    fix requirements

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+""" read_file function """
+
+
 def read_file(filename=""):
     """ Reads a text file (UTF8) and prints it to stdout """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding='utf-8') as file:
+        print(file.read(), end="")
