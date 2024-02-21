791838752a28d4526662f4d54090a6e4226ef047
git-alexis <alex59.billemont@gmail.com>
2024-02-21 14:41:22 +0100

    Add Rectangle class that inherits from Base with a width, height, x, and y attributes

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+""" Rectangle class """
+
+
+from models.base import Base
+
+class Rectangle(Base):
+
+    """ Rectangle class """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes the Rectangle class with an id, width, height, x, and y attributes
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ Returns the width of the rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Sets the width of the rectangle """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Returns the height of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height of the rectangle """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Returns the x value of the rectangle """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Sets the x value of the rectangle """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Returns the y value of the rectangle """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Sets the y value of the rectangle """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
