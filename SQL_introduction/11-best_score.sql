98b747475801fdcf431f0cfa506b83aee701331b
git-alexis <alex59.billemont@gmail.com>
2024-03-20 18:20:43 +0100

    change order of condition

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- List all records (score and name) of the table second_table
 -- where score is greater or equal to 10 and by descending order of score
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List all records (score, name) of the second_table table
 -- where the name field is not NULL
-SELECT score, name FROM second_table ORDER BY score DESC WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
