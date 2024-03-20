664071a3b099e64ea808b20f50e260939f66409e
git-alexis <alex59.billemont@gmail.com>
2024-03-20 17:33:08 +0100

    Add SQL query to list records with score >= 10 in descending order

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List all records (score and name) of the table second_table
+-- where score is greater or equal to 10 and by descending order of score
+SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
