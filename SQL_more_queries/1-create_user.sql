13542e83a55599608d4a70139391afe4aef46d86
git-alexis <alex59.billemont@gmail.com>
2024-03-22 10:12:41 +0100

    delete identified by on grant line

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- Create new user user_0d_1 if he doesn't exit with password user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Give all privileges to user_0d_1 and update them
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Refreshes the privileges
 FLUSH PRIVILEGES;
