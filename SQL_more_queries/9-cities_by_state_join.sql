8fbf13c60420aaf45029d10cf0fc174031c29109
git-alexis <alex59.billemont@gmail.com>
2024-03-22 11:29:07 +0100

    Add SQL query to list cities by state join

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- List all cities contained in the database hbtn_0e_4_usa
+-- by join between the cities and states tables
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
