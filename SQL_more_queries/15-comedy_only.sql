5479a3ab60050e13d66a9eca05145d13f17b3fe1
git-alexis <alex59.billemont@gmail.com>
2024-03-22 13:35:14 +0100

    Add SQL query to list all Comedy shows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- List all Comedy shows in the database
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_genres ON tv_shows.id = tv_genres.show_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
