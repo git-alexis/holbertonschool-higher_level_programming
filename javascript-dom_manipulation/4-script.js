454b62454b31f6aee1b0ed440f5e380f8e3e3117
git-alexis <alex59.billemont@gmail.com>
2024-05-28 11:20:12 +0200

    change my_list by ul.my_list valid CSS selector

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,5 +1,5 @@
 document.getElementById('add_item').addEventListener('click', function() {
-  const ul = document.querySelector('my_list');
+  const ul = document.querySelector('ul.my_list');
   const li = document.createElement('li');
   li.textContent = 'Item';
   ul.appendChild(li);
